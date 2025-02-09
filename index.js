25f3706bb30ba84f47b321a910405d5ab545ae1b
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-02-09 11:06:35 +0200

    Асинхронізація Even loop

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 'use strict'
+
+/*
+Event loop - це механізм, який керує виконанням коду
+
+Концептуально, ідея Event loop полягає в тому,
+щоб не заблокувати основний потік даних.
+
+Технічно, ця концепція реалізується через Coogle Chrome,
+який написан на програмі С++
+
+*/
+const btn = document.querySelector('#btn');
+
+btn.addEventListener('click',clickHandler);
+function clickHandler(event){
+     console.log('BUTTON');
+}
+
