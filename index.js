3a0f13b5b894cd434109cf9e237cd6ad20fefdb5
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-02-11 21:34:34 +0200

    Add promis,fetch,then,catch,finally

--- index.js
+++ index.js
@@ -1,72 +1,25 @@
 "use strict";
 
-const user = {
-  firstName: "Nataliia",
-  lastName: "Yakovenko",
-  age: 41,
-  footSize: 36,
-  adress:{
-    street:'D.Vishneveckiy',
-    numberHouse: 36,
-    flate: 30
-  },
-  sayHello: function(){ //в JSON функція не потрапить, вона будить ігноруватися
-
-    console.log('hello');
-  }
-};
-//JSON це позначення об'єктів JS,для перекладання даних в JSON, 
- //щоб ці дані можна було прочитати на інших мовах програмування
-
-// метод stringify() - перетворює об'єкт JS в JSON
-//Серіалізація - це процес перетворення JS об'єктів або даних в JSON
+fetch("./user.json")
+  .then((response) => {
+    return response.json(); // перетворюємо 0 і 1 на js об'єкт
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error)=>{
+    console.log(error);
+  })
+  .finally(()=>{
+    console.log('finally quastion closed')
+  });
+//----------------------------------------------------
 
 
-//метод parse() - перетворює об'єкт JSON в об'єкт JS 
-//Десеріалізація - це процес перетворення JSON об'єктів або даних в JS об'єкт
-
-console.log(user);
-const jsonString = JSON.stringify(user)  //перетворюємо об'єкт JS в JSON
-console.log(jsonString);
-
-const jsonParse = JSON.parse(jsonString) //перетворюєм об'єкт JSON в об'єкт JS 
-console.log(jsonParse);
-//---------------------------------------------------------------
-
-/*Поверхнева копія об'єкту (shallow copy) - це копія, де копіюються тільки посилання на елементи,
-а не самі значення
-*/
-
-const originalObjact = {
-  users:[
-    {
-      firstName: 'Nataliia',
-      age: 41,
-    },
-    {
-      firstName: 'Roland',
-      age: 27,
-    }
-  ]
-}
-//ми можемо використати spred оператор (...)
-const shallowCopy = {...originalObjact} //цей підхід дозволяє нам зробити новий об'єкт
-
-console.log(originalObjact);
-console.log(shallowCopy);
-console.log(originalObjact.users[0].firstName ="Nataliia jan");
-console.log(shallowCopy);
-//----------------------------------------------------------------------------
-
-//Глибока копія (deep copy) - це створення повноцінної копії об'єкта,де копіюються посилання
- //на його елементи і самі значення.
- //Тоєсть при внесенні будь-яких змін в один об'єкт, копія об'єкту залишиться не змінною 
-
- const deepCopy = JSON.parse(JSON.stringify(originalObjact));
- console.log(deepCopy);
-console.log(originalObjact.users[1].firstName ="Roland jan");
-console.log(originalObjact);
-console.log(deepCopy);
-console.log(deepCopy.users[0].age=42);
-console.log(deepCopy);
-console.log(originalObjact);
+  fetch("https://api.monobank.ua/bank/currency")
+  .then((response) => {
+    return response.json(); 
+  })
+  .then((data) => {
+    console.log(data);
+  });
