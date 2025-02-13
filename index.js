d33135896ce920d4e0649c6e2f5070a8a0827bd4
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-02-13 21:06:49 +0200

    Add in loadImage Timeout

--- index.js
+++ index.js
@@ -7,41 +7,50 @@ function loadImage(src) {
 
   //2. Створюємо елемент у верстці, який буде слугувати нам для того,
   // щоб вказувати, чи завантажуємо ми щось чи ні
-  const h2 = document.createElement('h2');
-  h2.id = 'loading-h2';
-  h2.append('loading image......')
+  const h2 = document.createElement("h2");
+  h2.id = "loading-h2";
+  h2.append("loading image......");
   document.body.append(h2);
 
   //3. Повертаємо promise
   //Промісифікаці - коли ми огортаємо якийсь асинхронний код в promise
   //для забезпечення собі зручну роботу
   return new Promise((resolve, regect) => {
+    //Створюємо таймаут на 5 секунд для завантаження картинки
+    const timeoutId = setTimeout(() => {
+      regect("Time to download is up");
+    }, 3000);
+
     //Підписуємо створену картинку на подію load
     //якщо картинка завантажиться - ми resolve promise з елементом картинки
     img.addEventListener("load", () => {
+      clearTimeout(timeoutId);
       resolve(img);
     });
 
     //Підписуємо створену картинку на подію error
     //якщо картинка не завантажиться - ми regect promise з повідомленням про помилку
     img.addEventListener("error", () => {
+      clearTimeout(timeoutId);
       regect("Image can`t be loaded");
     });
   });
 }
 
 loadImage(
-  'https://www.akc.org/wp-content/uploads/2018/05/Three-Australian-Shepherd-puppies-sitting-in-a-field.jpg')
-  .then((img)=>{
-    document.body.append(img);
-  },(errorMessage)=>{
-    const h2 = document.createElement('h2');
-    h2.append(errorMessage);
-    document.body.append(h2);
-  })
-  .finally(()=>{
-    const loadingH2 = document.querySelector('#loading-h2')
+  "https://www.akc.org/wp-content/uploads/2018/05/Three-Australian-Shepherd-puppies-sitting-in-a-field.jpg"
+)
+  .then(
+    (img) => {
+      document.body.append(img);
+    },
+    (errorMessage) => {
+      const h2 = document.createElement("h2");
+      h2.append(errorMessage);
+      document.body.append(h2);
+    }
+  )
+  .finally(() => {
+    const loadingH2 = document.querySelector("#loading-h2");
     loadingH2.remove();
-
-  })
-
+  });
