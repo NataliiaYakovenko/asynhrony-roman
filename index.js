2ae4c4af9d22992a549bef62df8066d53ad90a82
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-02-25 21:37:24 +0200

    Agg async / await

--- index.js
+++ index.js
@@ -1,65 +1,37 @@
 "use strict";
 
 /*
-https://api.openweathermap.org/data/2.5/weather?q=Zaporizhzhia&appid=f7c576ba3699bdd0b98ddcf196639992&units=metric
+async
+await
 
-f7c576ba3699bdd0b98ddcf196639992
-
-Задача
-Зробити погодний віджет
-Алгоритм вирішення:
-- Зробити верстку елементів, які отримують від користувача дані про місто
-- Отримати дані з API і обробити їх (підготувати дані для відмалювання у вертці)
-- Зробити карту з погодою і відобразити її
 */
 
-const API_KEY = "f7c576ba3699bdd0b98ddcf196639992";
-const API_BASE = "https://api.openweathermap.org/data/2.5/weather";
+const URL = 'https://randomuser.me/api/';
 
-const btn = document.querySelector(".btn");
+function loadDataWithoutAsync(url){
+  const p =fetch(url);
 
-btn.addEventListener("click", buttonClickHandler);
+  const jsonResponse = p.then((response)=>{
+    return response.json()
+  })
+  jsonResponse.then((data)=>{
+    console.log(data);
 
-function buttonClickHandler({ target }) {
-  const selectValue = target.previousElementSibling.value;
-  console.log(selectValue);
-  requestApi(selectValue);
-}
-
-function requestApi(cityName) {
-  //Готуємо url
-  const url = `${API_BASE}?q=${cityName}&appid=${API_KEY}&units=metric`;
-  console.log(url);
-
-  //Робимо запит
-  fetch(url)
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-      //Відмальовуємо погоду
-      displayWeather(data)
-    
-    });
-}
-
-
-function displayWeather(weatherObject){
-  const{name,main:{temp},weather:[{description}]}=weatherObject
-
-  const article = document.querySelector('#weather-box');
-  article.classList.add('weather-display');
-
-  const city = document.querySelector('#city');
-  city.textContent= name;
-
-  const temperature = document.querySelector('#temp');
-  temperature.textContent= `${temp} ℃`;
-
-  const descrip = document.querySelector('#description');
-  descrip.textContent = description;
-
-
+  })
 
 }
+
+// loadDataWithoutAsync(URL)
+//--------------------------------------------------------------
+
+//ASYNC / AWAIT
+
+async function loadDataWithoutAsync(url) {
+    const p = fetch(url);
+    const response = await p;
+    const data = await response.json()
+
+    console.log(data);
+}
+
+loadDataWithoutAsync(URL)
